7885ce014f60c3bcf606f6a9dc6e91ff85c546af
ania-wis <20217@student.ans-elblag.pl>
2023-12-10 18:54:52 +0100

    ostatnia poprawka, mam nadzieję xd

--- next.config.js
+++ next.config.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ["i.dummyjson.com"]
-  }
+  },
   env:{
     strie_public_key: process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY,
   }
