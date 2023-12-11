25d80ac848dadc316cd79828c1898d6deaf55842
knickW <konrad.warchol7@gmail.com>
2023-12-11 19:20:58 +0100

    test

---
+++ src/components/products/ProductModel.ts
@@ -0,0 +1,34 @@
+import mongoose, { Schema, model, Document } from 'mongoose';
+
+interface IProduct extends Document {
+  id: number;
+  title: string;
+  description: string;
+  price: number;
+  discountPercentage: number;
+  rating: number;
+  stock: number;
+  brand: string;
+  category: string;
+  thumbnail: string;
+  images: string[];
+  quantity: number;
+}
+
+const productSchema = new Schema<IProduct>({
+  id: Number,
+  title: String,
+  description: String,
+  price: Number,
+  discountPercentage: Number,
+  rating: Number,
+  stock: Number,
+  brand: String,
+  category: String,
+  thumbnail: String,
+  images: [String],
+  quantity: Number,
+});
+
+export const Product = mongoose.model<IProduct>('Product', productSchema);
+export type { IProduct };
---
+++ src/data/database.ts
@@ -0,0 +1,47 @@
+﻿import { Product } from "../components/products/ProductModel";
+
+require('dotenv').config();
+const mongoose = require('mongoose');
+
+const mongoURI = process.env.dbURI;
+
+mongoose.connect(mongoURI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+})
+.then(async () => {
+  console.log('Connected to MongoDB');
+
+  const getAllProducts = async () => {
+    try {
+      console.log('Retrieving all products...');
+      const allProducts = await Product.find({}).exec(); // Załóż, że Product jest poprawnym modelem
+      console.log('All products:', allProducts);
+      return allProducts;
+    } catch (error) {
+      console.error('Error while fetching products:', error);
+      throw error;
+    }
+  };
+
+  const addNewProduct = async (newProductData: typeof Product) => {
+    try {
+      console.log('Adding a new product...');
+      const newProduct = new Product(newProductData); // Załóż, że Product jest poprawnym modelem
+      const savedProduct = await newProduct.save();
+      console.log('New product saved:', savedProduct);
+      return savedProduct;
+    } catch (error) {
+      console.error('Error while saving new product:', error);
+      throw error;
+    }
+  };
+  
+  module.exports = {
+    getAllProducts,
+    addNewProduct,
+  };
+})
+.catch((err: any) => {
+  console.error('Connection error', err);
+});
